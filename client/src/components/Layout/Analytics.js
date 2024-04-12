a5a232c0191a4ece0169c6bfd97435bbc0e3d0f9
gauravpandey03 <gauravpandey10082003@gmail.com>
2024-04-12 23:39:30 +0530

    category wise filter

--- client/src/components/Layout/Analytics.js
+++ client/src/components/Layout/Analytics.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { Progress } from "antd";
 const Analytics = ({ allTransection }) => {
+  // category
+  const categories = [
+    "salary",
+    "tip",
+    "project",
+    "food",
+    "movie",
+    "bills",
+    "medical",
+    "fee",
+    "tax",
+  ];
+
   // total transaction
   const totalTransaction = allTransection.length;
   const totalIncomeTransactions = allTransection.filter(
@@ -85,6 +98,60 @@ const Analytics = ({ allTransection }) => {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-md-4">
+          <h4>Categorywise Income</h4>
+          {categories.map((category) => {
+            const amount = allTransection
+              .filter(
+                (transaction) =>
+                  transaction.type === "income" &&
+                  transaction.category === category
+              )
+              .reduce((acc, transaction) => acc + transaction.amount, 0);
+            return (
+              amount > 0 && (
+                <div className="card">
+                  <div className="card-body">
+                    <h5>{category}</h5>
+                    <Progress
+                      percent={((amount / totalIncomeTurnover) * 100).toFixed(
+                        0
+                      )}
+                    />
+                  </div>
+                </div>
+              )
+            );
+          })}
+        </div>
+        <div className="col-md-4">
+          <h4>Categorywise Expense</h4>
+          {categories.map((category) => {
+            const amount = allTransection
+              .filter(
+                (transaction) =>
+                  transaction.type === "expense" &&
+                  transaction.category === category
+              )
+              .reduce((acc, transaction) => acc + transaction.amount, 0);
+            return (
+              amount > 0 && (
+                <div className="card">
+                  <div className="card-body">
+                    <h5>{category}</h5>
+                    <Progress
+                      percent={((amount / totalExpenseTurnover) * 100).toFixed(
+                        0
+                      )}
+                    />
+                  </div>
+                </div>
+              )
+            );
+          })}
         </div>
       </div>
     </>
